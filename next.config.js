de19d33ad94915cf7223932dd4b4916db1b4d6ca
Calvin Chan <xLysergia@gmail.com>
2022-05-27 02:00:43 +0100

    added ignore typescript errors temporarily to allow for vercel deployment

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
 module.exports = {
   reactStrictMode: true,
 
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+
   async redirects() {
     return [
       {
